f540c05340e70d531939dd96db90e9067a8d979f
Shalini Reddy Bommana <Shalinireddy_B@ad.infosys.com>
2017-09-13 19:12:03 +0530

    added about and features pages

--- jade-build.js
+++ jade-build.js
@@ -1,8 +1,22 @@
-var template = require("jade").compileFile(__dirname+"/source/templates/homepage.jade"),
+var homepage = require("jade").compileFile(__dirname+"/source/templates/homepage.jade"),
+    aboutpage = require("jade").compileFile(__dirname+"/source/templates/about.jade"),
+	aboutpage = require("jade").compileFile(__dirname+"/source/templates/features.jade"),
     fs=require("fs");
 
-fs.writeFile(__dirname+"/docs/index.html",template({title:'Home'}),function(err,data){
+fs.writeFile(__dirname+"/docs/index.html",homepage({title:'Home'}),function(err,data){
 	if (err) throw err;
-	console.log('The file has been saved!');
+	console.log('The home file has been saved!');
+	
+});	
+
+fs.writeFile(__dirname+"/docs/about/index.html",aboutpage({title:'About Simplity'}),function(err,data){
+	if (err) throw err;
+	console.log('The about file has been saved!');
+	
+});	
+
+fs.writeFile(__dirname+"/docs/features/index.html",aboutpage({title:'Features'}),function(err,data){
+	if (err) throw err;
+	console.log('The features file has been saved!');
 	
 });	
