b24b4d7e4f467f881a67997c881aebc2b0279c27
Chetana Amancharla <Chetana_Shanbhag@ad.infosys.com>
2017-09-14 10:50:13 +0530

    fix for path issues

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express=require("express"),
 	
 	
 app.use(logger('dev'))
-app.use(express.static(__dirname+'/docs'))	
+app.use('/portal',express.static(__dirname+'/docs'))	
 
 app.listen(process.env.PORT || 3000,function(){
 	console.log('Listening on http://localhost ' + (process.env.PORT || 3000))
