6c950b8bab0407b7e4bafbf8d0d136b29332f56f
Shalini Reddy Bommana <Shalinireddy_B@ad.infosys.com>
2017-09-18 18:39:28 +0530

    examples page added

--- jade-build.js
+++ jade-build.js
@@ -1,6 +1,7 @@
 var homepage = require("jade").compileFile(__dirname+"/source/templates/homepage.jade"),
     aboutpage = require("jade").compileFile(__dirname+"/source/templates/about.jade"),
 	featurespage = require("jade").compileFile(__dirname+"/source/templates/features.jade"),
+	examplespage = require("jade").compileFile(__dirname+"/source/templates/examples.jade"),
     fs=require("fs");
 
 fs.writeFile(__dirname+"/docs/index.html",homepage({title:'Home'}),function(err,data){
@@ -18,5 +19,11 @@ fs.writeFile(__dirname+"/docs/about/index.html",aboutpage({title:'About Simplity
 fs.writeFile(__dirname+"/docs/features/index.html",featurespage({title:'Features'}),function(err,data){
 	if (err) throw err;
 	console.log('The features file has been saved!');
+	
+});	
+
+fs.writeFile(__dirname+"/docs/examples/index.html",examplespage({title:'examples'}),function(err,data){
+	if (err) throw err;
+	console.log('The examples file has been saved!');
 	
 });	
