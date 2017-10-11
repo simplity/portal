b09e9285b4281e088e7c9960965edee2c0838431
Shalini Reddy Bommana <Shalinireddy_B@ad.infosys.com>
2017-10-11 13:40:21 +0530

    downloads and  getstarted pages added

--- jade-build.js
+++ jade-build.js
@@ -2,6 +2,8 @@ var homepage = require("jade").compileFile(__dirname+"/source/templates/homepage
     aboutpage = require("jade").compileFile(__dirname+"/source/templates/about.jade"),
 	featurespage = require("jade").compileFile(__dirname+"/source/templates/features.jade"),
 	examplespage = require("jade").compileFile(__dirname+"/source/templates/examples.jade"),
+	startpage = require("jade").compileFile(__dirname+"/source/templates/getStarted.jade"),
+	downloadspage = require("jade").compileFile(__dirname+"/source/templates/downloads.jade"),
     fs=require("fs");
 
 fs.writeFile(__dirname+"/docs/index.html",homepage({title:'Home'}),function(err,data){
@@ -25,5 +27,15 @@ fs.writeFile(__dirname+"/docs/features/index.html",featurespage({title:'Features
 fs.writeFile(__dirname+"/docs/examples/index.html",examplespage({title:'examples'}),function(err,data){
 	if (err) throw err;
 	console.log('The examples file has been saved!');
+	
+});	
+fs.writeFile(__dirname+"/docs/downloads/index.html",downloadspage({title:'downloads'}),function(err,data){
+	if (err) throw err;
+	console.log('The downloads file has been saved!');
+	
+});	
+fs.writeFile(__dirname+"/docs/getStarted/index.html",startpage({title:'getStarted'}),function(err,data){
+	if (err) throw err;
+	console.log('The getStarted file has been saved!');
 	
 });	
