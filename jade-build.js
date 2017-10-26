fddb455e1ba48849a2ea22bf4904ec0b98b28217
Madhavan_N <Madhavan_N@BLRKEC123980L>
2017-10-26 19:15:18 +0530

    updated getStarted

--- jade-build.js
+++ jade-build.js
@@ -4,6 +4,13 @@ var homepage = require("jade").compileFile(__dirname+"/source/templates/homepage
 	docspage = require("jade").compileFile(__dirname+"/source/templates/docs.jade"),
 	examplespage = require("jade").compileFile(__dirname+"/source/templates/examples.jade"),
 	startpage = require("jade").compileFile(__dirname+"/source/templates/getStarted.jade"),
+	gettingstartpage = require("jade").compileFile(__dirname+"/source/templates/gettingStarted.jade"),
+	plainwebservicepage = require("jade").compileFile(__dirname+"/source/templates/getStarted/plainWebService.jade"),
+	webservicesopenapipage = require("jade").compileFile(__dirname+"/source/templates/getStarted/webServicesOpenApi.jade"),
+	simplityservicewithjavapage = require("jade").compileFile(__dirname+"/source/templates/getStarted/simplityServiceWithJava.jade"),
+	simplityspringmvcpage = require("jade").compileFile(__dirname+"/source/templates/getStarted/simplitySpringMvc.jade"),
+	simplityspringbootpage = require("jade").compileFile(__dirname+"/source/templates/getStarted/simplitySpringBoot.jade"),
+	simplitybatchpage = require("jade").compileFile(__dirname+"/source/templates/getStarted/simplityBatch.jade"),
 	webappwithdbpage = require("jade").compileFile(__dirname+"/source/templates/getStarted/webappwithDb.jade"),
 	recordgenerationpage = require("jade").compileFile(__dirname+"/source/templates/getStarted/recordGeneration.jade"),
 	downloadspage = require("jade").compileFile(__dirname+"/source/templates/downloads.jade"),
@@ -50,6 +57,36 @@ fs.writeFile(__dirname+"/docs/getStarted/index.html",startpage({title:'getStarte
 	console.log('The getStarted file has been saved!');
 	
 });	
+fs.writeFile(__dirname+"/docs/getStarted/plainWebService/index.html",plainwebservicepage({title:'plainWebService'}),function(err,data){
+	if (err) throw err;
+	console.log('The plainWebService file has been saved!');
+	
+});	
+fs.writeFile(__dirname+"/docs/getStarted/webServicesOpenApi/index.html",webservicesopenapipage({title:'webServicesOpenApi'}),function(err,data){
+	if (err) throw err;
+	console.log('The webServicesOpenApi file has been saved!');
+	
+});	
+fs.writeFile(__dirname+"/docs/getStarted/simplityServiceWithJava/index.html",simplityservicewithjavapage({title:'simplityServiceWithJava'}),function(err,data){
+	if (err) throw err;
+	console.log('The simplityServiceWithJava file has been saved!');
+	
+});	
+fs.writeFile(__dirname+"/docs/getStarted/simplitySpringMvc/index.html",simplityspringmvcpage({title:'simplitySpringMvc'}),function(err,data){
+	if (err) throw err;
+	console.log('The simplitySpringMvc file has been saved!');
+	
+});	
+fs.writeFile(__dirname+"/docs/getStarted/simplitySpringBoot/index.html",simplityspringbootpage({title:'simplitySpringBoot'}),function(err,data){
+	if (err) throw err;
+	console.log('The simplitySpringBoot file has been saved!');
+	
+});
+fs.writeFile(__dirname+"/docs/getStarted/simplityBatch/index.html",simplitybatchpage({title:'simplityBatch'}),function(err,data){
+	if (err) throw err;
+	console.log('The simplityBatch file has been saved!');
+	
+});
 fs.writeFile(__dirname+"/docs/examplesDesc/mcHelloWorld.html",examplesDescpage1({title:'helloworld'}),function(err,data){
 	if (err) throw err;
 	console.log('The mcHelloWorld file has been saved!');
@@ -58,6 +95,11 @@ fs.writeFile(__dirname+"/docs/examplesDesc/mcHelloWorld.html",examplesDescpage1(
 fs.writeFile(__dirname+"/docs/examplesDesc/petClinic.html",examplesDescpage2({title:'petclinic'}),function(err,data){
 	if (err) throw err;
 	console.log('The petClinic file has been saved!');
+	
+});	
+fs.writeFile(__dirname+"/docs/gettingStarted/index.html",gettingstartpage({title:'Get Started'}),function(err,data){
+	if (err) throw err;
+	console.log('The Web App with Database file has been saved!');
 	
 });	
 fs.writeFile(__dirname+"/docs/getStarted/webappwithDb/index.html",webappwithdbpage({title:'Web App with Database'}),function(err,data){
