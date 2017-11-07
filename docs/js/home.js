4de920938c5251e559f98103063ae53d2856f95a
Madhavan_N <Madhavan_N@BLRKEC123980L>
2017-11-07 15:24:51 +0530

    updated getstarted

--- docs/js/home.js
+++ docs/js/home.js
@@ -26,3 +26,33 @@ function moveLeft(){
 	   left--;
     document.getElementsByClassName("componentPartition hidden")[left].className="componentPartition";	
 }
+
+function toClipboard(copyContentClassName) {
+	var contentToCopyElement = document.getElementById(copyContentClassName);
+	
+	var copyTarget = document.createElement("textarea");
+	copyTarget.style.position = "absolute";
+	copyTarget.style.left = "-9999px";
+	copyTarget.style.top = "0";
+	copyTarget.id = "_hiddenCopyText_";
+	document.body.appendChild(copyTarget);
+	
+	copyTarget.textContent = contentToCopyElement.textContent;
+	
+	var currentElement = document.activeElement;
+	copyTarget.focus();
+	copyTarget.setSelectionRange(0, copyTarget.value.length);
+	
+	var copySelectedElement;
+	try {
+		copySelectedElement = document.execCommand("copy");
+	} catch (e) {
+		copySelectedElement = false;
+	}
+	
+	if (currentElement && typeof currentElement.focus === "function") {
+		currentElement.focus();
+	}
+	copyTarget.textContent = "";
+	return copySelectedElement;
+}
